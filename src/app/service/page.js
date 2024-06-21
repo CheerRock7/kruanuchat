f2f2c70f216666cd7221ef3c3e1aa8f04dbadf1e
Chetsadawut <azathos000@gmail.com>
2024-06-21 12:51:27 +0700

    first commit

---
+++ src/app/service/page.js
@@ -0,0 +1,17 @@
+import Link from "next/link"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function service() {
+  return (
+    <>
+    <h1><center>Home Page</center></h1>
+    <br></br>
+    <center>
+    <Link href="/">Home</Link> |
+    <Link href="/about">About</Link> |
+    <Link href="/service">Service</Link> | 
+    <Link href="/contact">Contact</Link> |
+    </center>
+    </>
+  );
+}
